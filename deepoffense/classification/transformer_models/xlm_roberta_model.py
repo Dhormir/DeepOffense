079a0bec17bf7d959dde26b54c970e2af88c4c02
Domingo <domingo.benoit@alumnos.inf.utfsm.cl>
2024-08-20 20:24:25 -0400

    Added missing torch import

--- deepoffense/classification/transformer_models/xlm_roberta_model.py
+++ deepoffense/classification/transformer_models/xlm_roberta_model.py
@@ -3,7 +3,7 @@ from transformers.models.xlm_roberta.modeling_xlm_roberta import (
     XLM_ROBERTA_PRETRAINED_MODEL_ARCHIVE_LIST,
 )
 from typing import List, Optional, Tuple, Union
-
+import torch
 
 from deepoffense.classification.transformer_models.roberta_model import RobertaForSequenceClassification
 
